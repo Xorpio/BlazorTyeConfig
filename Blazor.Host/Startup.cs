e2624418a1ca71278939e0e148c2bd41bb702a36
Xorpio <3060868+Xorpio@users.noreply.github.com>
2021-03-06 05:33:25 +0100

    added endpoints to hosting app

--- Blazor.Host/Startup.cs
+++ Blazor.Host/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,6 +13,13 @@ namespace Blazor.Host
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -41,6 +49,14 @@ namespace Blazor.Host
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapFallbackToFile("index.html");
+                endpoints.MapGet("/endpoints.json", async context =>
+                {
+                    await context.Response.WriteAsJsonAsync(new
+                    {
+                        auth = Configuration.GetServiceUri("backend:https"),
+                        admin = Configuration.GetServiceUri("blazor-host")
+                    });
+                });
             });
         }
     }
